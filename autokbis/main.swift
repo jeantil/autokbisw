f8b42850c96541b130b7e07484c6732ca60f3651
Jean Helou <jean.helou@gmail.com>
2016-11-22 15:20:25 +0100

    Initial Commit

---
+++ autokbis/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  autokbis
+//
+//  Created by Jean Helou on 22/11/2016.
+//  Copyright © 2016 Jean Helou. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
