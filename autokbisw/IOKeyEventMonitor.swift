027b8c98c8edcdcb4d43e67a4e85183bb1e6404d
Jean Helou <jean.helou@gmail.com>
2016-11-24 15:13:09 +0100

    Segfault
    http://stackoverflow.com/questions/40788007/memorizing-the-last-seen-sender-of-iokit-iohidevents-in-swift-3?noredirect=1#comment68799412_40788007

--- autokbisw/IOKeyEventMonitor.swift
+++ autokbisw/IOKeyEventMonitor.swift
@@ -12,36 +12,62 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
-
 import Foundation
+import IOKit
+import IOKit.usb
+import IOKit.hid
+
+struct IOKeyEventMonitorContext{
+  var lastSeenSender:String
+  init(lastSeenSender:String){
+    self.lastSeenSender=lastSeenSender
+  }
+}
 
 class IOKeyEventMonitor{
-    private
-    let hidManager:IOHIDManager
-    let match:CFMutableDictionary
-
-    private class func createDeviceMatchingDictionary( usagePage:Int,  usage:Int ) -> CFMutableDictionary {
-        let dict = [
-            kIOHIDDeviceUsageKey:usage,
-            kIOHIDDeviceUsagePageKey:usagePage
-            ] as NSDictionary
-
-        return dict.mutableCopy() as! NSMutableDictionary;
-    }
-
+  private
+  let hidManager:IOHIDManager
+  let match:CFMutableDictionary
+  var lastSeenSender:String=""
+  
+  private class func createDeviceMatchingDictionary( usagePage:Int,  usage:Int ) -> CFMutableDictionary {
+    let dict = [
+      kIOHIDDeviceUsageKey:usage,
+      kIOHIDDeviceUsagePageKey:usagePage
+      ] as NSDictionary
+    
+    return dict.mutableCopy() as! NSMutableDictionary;
+  }
+  
+  init? ( usagePage:Int,  usage:Int ){
+    hidManager = IOHIDManagerCreate( kCFAllocatorDefault, IOOptionBits(kIOHIDOptionsTypeNone) );
+    match=IOKeyEventMonitor.createDeviceMatchingDictionary(usagePage:usagePage, usage:usage);
+    IOHIDManagerSetDeviceMatching( hidManager, match );
+  }
+  deinit {
+    // FIXME find out how to pass nil as an IOKit.IOKit.IOHIDValueCallback to unregister the callback
+    //    IOHIDManagerRegisterInputValueCallback( hidManager, nil as IOKit.IOHIDValueCallback, nil);
+  }
+  func start()-> Void {
+    
     let myHIDKeyboardCallback: IOHIDValueCallback={
-        (context, ioreturn, sender, value) in
-        let sender = Unmanaged<IOKeyEventMonitor>.fromOpaque(sender!).takeUnretainedValue()
-        print("sender \(sender)")
+      (context, ioreturn, sender, value) in
+      let selfPtr = Unmanaged<IOKeyEventMonitor>.fromOpaque(context!).takeUnretainedValue();
+      let sender = Unmanaged<IOKeyEventMonitor>.fromOpaque(sender!).takeUnretainedValue();
+      let senderStr = String(describing:sender);
+      
+      if(selfPtr.lastSeenSender != senderStr){
+        print("sender \(context!)");
+        print("sender \(sender)");
+        selfPtr.lastSeenSender = senderStr;
+      }
     }
-    init? ( usagePage:Int,  usage:Int ){
-        hidManager = IOHIDManagerCreate( kCFAllocatorDefault, IOOptionBits(kIOHIDOptionsTypeNone) );
-        match=IOKeyEventMonitor.createDeviceMatchingDictionary(usagePage:usagePage, usage:usage);
-        IOHIDManagerSetDeviceMatching( hidManager, match );
-        IOHIDManagerRegisterInputValueCallback( hidManager, myHIDKeyboardCallback, nil );
-        IOHIDManagerScheduleWithRunLoop( hidManager, CFRunLoopGetMain(), CFRunLoopMode.defaultMode!.rawValue);
-        IOHIDManagerOpen( hidManager,  IOOptionBits(kIOHIDOptionsTypeNone) );
-    }
-
-
+    
+    let context = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque());
+    print("start context \(context)");
+    IOHIDManagerRegisterInputValueCallback( hidManager, myHIDKeyboardCallback, nil);
+    IOHIDManagerScheduleWithRunLoop( hidManager, CFRunLoopGetMain(), CFRunLoopMode.defaultMode!.rawValue);
+    IOHIDManagerOpen( hidManager,  IOOptionBits(kIOHIDOptionsTypeNone) );
+  }
+  
 }
